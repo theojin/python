3161114b8bcf58caeb8a99b0350240fe128258b7
Jin Yoon <theojini@gmail.com>
2017-07-28 22:11:42 +0900

    Test setup.py
    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'module_name',
+    version = '1.0.0',
+    py_modules = ['module_names'],
+    author = 'Jin Yoon',
+    author_email = 'theojini@gmail.com',
+    url = 'http://storycompiler.com',
+    description = 'Test',
+    )
